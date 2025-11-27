ce6c1de2e4237e9a42a19b41f69978eb6ddb32ca
ZeayW <48197878+ZeayW@users.noreply.github.com>
2025-11-27 10:57:07 +0800

    update the codes for the local task

---
+++ src/dataset_gcl.py
@@ -0,0 +1,127 @@
+import sys
+
+sys.path.append("..")
+
+import dgl
+from dgl.data import DGLDataset
+import networkx as nx
+import torch as th
+import numpy as np
+import os
+import random
+from single_verilog_parser import *
+from options import get_options
+from torch.nn.parameter import Parameter
+from generate_data import *
+import pickle
+import tee
+
+
+options = get_options()
+
+
+
+def parse_single_file(nodes,edges,output_node):
+    # nodes: list of (node, {"type": type}) here node is a str ,like 'n123' or '1'b1'
+    # note that here node type does not include buf /not
+    label2id = {'NAND': 0, 'AND': 1,
+                'OR': 2,  'INV': 3, 'NOR': 4, 'XOR': 5, 'MUX': 6, 'XNOR': 7,
+                'MAJ': 8, 'PI': 9}
+    #print(nodes)
+    #print(edges)
+    nid = 0
+    node2id = {}
+    id2node = {}
+    ntype = th.zeros((len(nodes), len(label2id.keys())), dtype=th.float)
+    for n in nodes:
+        if node2id.get(n[0]) is None:
+            node2id[n[0]] = nid
+            id2node[nid] = n[0]
+            type = n[1]["type"]
+            if re.search("\d", type):
+                type = type[: re.search("\d", type).start()]
+            ntype[nid][label2id[type]] = 1
+            nid += 1
+
+    src_nodes = []
+    dst_nodes = []
+    for src, dst, edict in edges:
+        src_nodes.append(node2id[src])
+        dst_nodes.append(node2id[dst])
+    #print(src_nodes,dst_nodes)
+
+    graph = dgl.graph(
+        (th.tensor(src_nodes), th.tensor(dst_nodes)), num_nodes=len(node2id)
+    )
+    graph.ndata["ntype"] = ntype
+    graph.ndata['ntype2'] = th.argmax(ntype, dim=1).squeeze(-1)
+    topo = dgl.topological_nodes_generator(graph)
+
+    return graph,topo
+
+
+class Aug_graph:
+    def __init__(self,graph,topo,PO,size):
+        self.graph = graph
+        self.topo = topo
+        self.PO =PO
+        self.size = size
+
+if __name__ == "__main__":
+    options = get_options()
+
+    save_path = options.datapath
+    if not os.path.exists(os.path.join(save_path, 'i{}'.format(options.num_input))):
+        os.makedirs(os.path.join(save_path, 'i{}'.format(options.num_input)))
+    stdout_f = os.path.join(save_path, "stdout.log")
+    stderr_f = os.path.join(save_path, "stderr.log")
+    with tee.StdoutTee(stdout_f), tee.StderrTee(stderr_f):
+        print(options)
+        th.multiprocessing.set_sharing_strategy('file_system')
+        device = th.device("cuda:" + str(options.gpu) if th.cuda.is_available() else "cpu")
+
+
+        data_file = os.path.join(save_path, 'i{}/origin.pkl'.format(options.num_input))
+
+        if os.path.exists(data_file) is False:
+            rawdata_path = "../truthtables/i{}/implementation/".format(options.num_input)
+            th.multiprocessing.set_sharing_strategy('file_system')
+
+            orign_graphs = []
+            positive_pairs = [[], [], []]
+            filelist = os.listdir(rawdata_path)
+            print('#cases:',len(filelist))
+            for vf in filelist:
+                if not vf.endswith('.v') or not os.path.exists(os.path.join(rawdata_path, vf)):
+                    continue
+                print('\ngenerate positive samples for {}'.format(vf))
+                value = vf.split('_')[2].split('.')[0][1:]
+                parser = DcParser('i{}_v{}'.format(options.num_input, value))
+                output_node, nodes, edges = parser.parse(os.path.join(rawdata_path, vf))
+                if len(nodes) == 0:
+                    print('empty...')
+                    continue
+                original_graph, original_topo = parse_single_file(nodes, edges, output_node)
+                orign_graphs.append((original_graph, original_topo))
+
+                for num2replace in range(1, 4):
+                    positive_pair = [None,None]
+                    for i in range(2):
+                        print('generating positive sample{}, num replaced = {}'.format(i, num2replace))
+                        new_nodes, new_edges, output_nid = transform(nodes, edges, output_node, num2replace, options)
+                        new_graph, new_topo = parse_single_file(new_nodes, new_edges, output_nid)
+                        new_graph.ndata['v'] = int(value)*th.ones(len(new_nodes), dtype=th.float)
+                        new_graph.ndata['output'] = th.zeros(len(new_nodes), dtype=th.float)
+                        new_graph.ndata['output'][new_topo[-1]] = 1
+                        positive_pair[i] = [new_graph,new_topo[-1].item(),new_graph.number_of_nodes()] # graph, PO, size
+                    positive_pairs[num2replace - 1].append(positive_pair)
+            print(positive_pairs[0])
+            # print(dataset.batch_graph.ndata)
+
+            save_file = os.path.join(save_path, 'i{}/origin.pkl'.format(options.num_input))
+            with open(save_file, 'wb') as f:
+                pickle.dump(orign_graphs, f)
+            for j in range(3):
+                save_file = os.path.join(save_path, 'i{}/aug{}.pkl'.format(options.num_input, j + 1))
+                with open(save_file, 'wb') as f:
+                    pickle.dump(positive_pairs[j], f)
--- src/train_local.py
+++ src/train_local.py
@@ -12,8 +12,34 @@ import networkx as nx
 from random import shuffle
 import random
 import torch as th
-from MyDataLoader import *
+from MyDataLoader2 import *
 
+def DAG2UDG(g):
+    r"""
+
+    used to transform a (directed acyclic graph)DAG into a undirected graph
+
+    :param g: dglGraph
+        the target DAG
+
+    :return:
+        dglGraph
+            the output undirected graph
+    """
+    edges = g.edges()
+    reverse_edges = (edges[1],edges[0])
+    # add the reversed edges
+    new_edges = (th.cat((edges[0],reverse_edges[0])),th.cat((edges[1],reverse_edges[1])))
+    udg =  dgl.graph(new_edges,num_nodes=g.num_nodes())
+
+    # copy the node features
+    for key, value in g.ndata.items():
+        # print(key,value)
+        udg.ndata[key] = value
+    # copy the edge features
+    udg.edata['direction'] = th.cat((th.zeros(size=(1,g.number_of_edges())).squeeze(0),th.ones(size=(1,g.number_of_edges())).squeeze(0)))
+
+    return udg
 
 def get_reverse_graph(g):
     edges = g.edges()
@@ -67,8 +93,11 @@ def oversample(g,options,in_dim):
     lowbit_mask = g.ndata['position']<=3
     # unlabel the nodes in muldiv
     no_muldiv_mask = labels.squeeze(-1)!=-1
-    no_internal_mask = g.ndata['internal'].squeeze() == 0
-    mask = th.logical_and(no_muldiv_mask, no_internal_mask)
+    if options.mask:
+        no_internal_mask = g.ndata['internal'].squeeze() == 0
+        mask = th.logical_and(no_muldiv_mask,no_internal_mask)
+    else:
+        mask = no_muldiv_mask
     print('#nodes after masking',len(labels[mask]))
     nodes = th.tensor(range(g.num_nodes()))
     nodes = nodes[mask]
@@ -148,6 +177,10 @@ def load_data(options):
         train_g, _ = pickle.load(f)
         if options.function:
             train_g.ndata['h'] = th.ones((train_g.number_of_nodes(), options.hidden_dim), dtype=th.float)
+        mask0=train_g.ndata['ntype']==0
+        mask1=train_g.ndata['ntype']==1
+        mask2=train_g.ndata['ntype']==2
+        print(len(train_g.ndata['ntype'][mask0]),len(train_g.ndata['ntype'][mask1]),len(train_g.ndata['ntype'][mask2]))
         train_g.edata['r'] = train_g.edata['r'].squeeze()
         train_g.ndata['adder_o'][train_g.ndata['mul_o'] == 1] = -1
         # train_g.ndata['adder_o'][train_g.ndata['sub_o'] == 1] = -1
@@ -179,17 +212,46 @@ def load_data(options):
         out_nlayers = 1
     in_sampler = Sampler([None] * in_nlayers, include_dst_in_src=options.include)
     out_sampler = Sampler([None] * out_nlayers , include_dst_in_src=options.include)
-    nids = th.tensor(range(val_g.number_of_nodes()))
-    mask1 = val_g.ndata['adder_o'].squeeze(-1) != -1
-    mask2 = val_g.ndata['internal'].squeeze() == 0
-    mask = th.logical_and(mask1, mask2)
-    nids = nids[mask]
-    nids = nids.numpy().tolist()
-    shuffle(nids)
-    val_nids = nids
+
+    # if options.mask:
+    #     val_split_file = "val_nids_masked_div-1mul-1.pkl"
+    #     test_split_file = "test_nids_masked_div-1mul-1.pkl"
+    # else:
+    #     val_split_file = "val_nids_nomul.pkl"
+    #     test_split_file = "test_nids_nomul.pkl"
+    # if os.path.exists(os.path.join(options.datapath, val_split_file)):
+    #     with open(os.path.join(options.datapath, val_split_file), 'rb') as f:
+    #         val_nids = pickle.load(f)
+    #     with open(os.path.join(options.datapath, test_split_file), 'rb') as f:
+    #         test_nids = pickle.load(f)
+    # else:
+    if True:
+        nids = th.tensor(range(val_g.number_of_nodes()))
+        mask1 = val_g.ndata['adder_o'].squeeze(-1) != -1
+        if options.mask:
+            mask2 = val_g.ndata['internal'].squeeze() == 0
+            mask = th.logical_and(mask1, mask2)
+        else:
+            mask = mask1
+        nids = nids[mask]
+        nids = nids.numpy().tolist()
+        shuffle(nids)
+        val_nids = nids
+        #test_nids = nids
+        # val_nids = nids[:int(len(nids) / 10)]
+        # test_nids = nids[int(len(nids) / 10):]
+        #
+        # with open(os.path.join(options.datapath, val_split_file), 'wb') as f:
+        #     pickle.dump(val_nids, f)
+        # with open(os.path.join(options.datapath, test_split_file), 'wb') as f:
+        #     pickle.dump(test_nids, f)
 
     # create dataloader for training/validate dataset
-    graph_function = get_reverse_graph
+    if options.sage:
+        graph_function = DAG2UDG
+        out_sampler.include_dst_in_src = True
+    else:
+        graph_function = get_reverse_graph
 
     traindataloader = MyNodeDataLoader(
         False,
@@ -213,18 +275,18 @@ def load_data(options):
         shuffle=True,
         drop_last=False,
     )
-
-    testdataloader = MyNodeDataLoader(
-        True,
-        val_g,
-        graph_function(val_g),
-        test_nids,
-        in_sampler,
-        out_sampler,
-        batch_size=val_g.num_nodes(),
-        shuffle=True,
-        drop_last=False,
-    )
+    testdataloader = None
+    # testdataloader = MyNodeDataLoader(
+    #     True,
+    #     val_g,
+    #     graph_function(val_g),
+    #     test_nids,
+    #     in_sampler,
+    #     out_sampler,
+    #     batch_size=val_g.num_nodes(),
+    #     shuffle=True,
+    #     drop_last=False,
+    # )
 
     return traindataloader, valdataloader, testdataloader
 
@@ -297,13 +359,21 @@ def validate(loaders,label_name,device,model,Loss,beta,options):
                 out_blocks = [b.to(device) for b in out_blocks]
                 #print(out_blocks)
                 # get in input features
-                in_input_features = in_blocks[0].srcdata["h"]
-                out_input_features = out_blocks[0].srcdata["h"]
+                if not options.function:
+                    in_input_features = in_blocks[0].srcdata["ntype"]
+                    out_input_features = out_blocks[0].srcdata["ntype"]
+                else:
+                    in_input_features = in_blocks[0].srcdata["h"]
+                    out_input_features = out_blocks[0].srcdata["h"]
                 # the central nodes are the output of the final block
                 output_labels = in_blocks[-1].dstdata[label_name].squeeze(1)
                 total_num += len(output_labels)
                 # predict the labels of central nodes
                 label_hat = model(in_blocks, in_input_features, out_blocks, out_input_features)
+                # if options.abgnn:
+                #     label_hat = model(in_blocks, in_input_features, out_blocks, out_input_features)
+                # else:
+                #     label_hat = model(in_blocks, in_input_features)
                 pos_prob = nn.functional.softmax(label_hat, 1)[:, 1]
                 # adjust the predicted labels based on a given thredshold beta
                 pos_prob[pos_prob >= beta] = 1
@@ -350,7 +420,9 @@ def validate(loaders,label_name,device,model,Loss,beta,options):
 
 def train(model):
     print(options)
+    loss_thred = options.loss_thred
     th.multiprocessing.set_sharing_strategy('file_system')
+
     traindataloader, valdataloader, testdataloader = load_data(options)
     print("Data successfully loaded")
 
@@ -362,7 +434,14 @@ def train(model):
     optim = th.optim.Adam(
         model.parameters(), options.learning_rate, weight_decay=options.weight_decay
     )
+    # optim = th.optim.Adam(
+    #     model.parameters(), options.learning_rate, weight_decay=options.weight_decay
+    # )
     model.train()
+    if options.abgnn:
+        if model.GCN1 is not None: model.GCN1.train()
+        if model.GCN2 is not None: model.GCN2.train()
+
 
     pre_loss = 100
     stop_score = 0
@@ -385,15 +464,24 @@ def train(model):
             in_blocks = [b.to(device) for b in in_blocks]
             out_blocks = [b.to(device) for b in out_blocks]
             # get in input features
-            in_input_features = in_blocks[0].srcdata["h"]
-            out_input_features = out_blocks[0].srcdata["h"]
-
+            if not options.function:
+                in_input_features = in_blocks[0].srcdata["ntype"]
+                out_input_features = out_blocks[0].srcdata["ntype"]
+            else:
+                in_input_features = in_blocks[0].srcdata["h"]
+                out_input_features = out_blocks[0].srcdata["h"]
+            # if epoch>0:
+            #     print(in_input_features)
             # the central nodes are the output of the final block
             output_labels = in_blocks[-1].dstdata[label_name].squeeze(1)
             total_num += len(output_labels)
             # predict the labels of central nodes
 
             label_hat = model(in_blocks, in_input_features, out_blocks, out_input_features)
+            # if options.abgnn:
+            #     label_hat = model(in_blocks,in_input_features,out_blocks,out_input_features)
+            # else:
+            #     label_hat = model(in_blocks, in_input_features)
             if get_options().nlabels != 1:
                 pos_prob = nn.functional.softmax(label_hat, 1)[:, 1]
             else:
@@ -501,10 +589,10 @@ if __name__ == "__main__":
         model = model.to(device)
         #model.load_state_dict(th.load(model_save_path, map_location=th.device('cpu')))
         model.load_state_dict(th.load(model_save_path,map_location={'cuda:1':'cuda:0'}))
-        _, _, testdataloader = load_data(options)
+        traindataloader, valdataloader, testdataloader = load_data(options)
 
         Loss = nn.CrossEntropyLoss()
-        validate([testdataloader], 'adder_o', device, model,Loss, beta, options)
+        validate([valdataloader], 'adder_o', device, model,Loss, beta, options)
 
 
     elif options.checkpoint:
